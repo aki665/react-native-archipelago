39209be6320f36c270cc7352d0b7684d101e2277
aki665 <aki.virtanen000@gmail.com>
2024-06-19 15:56:04 +0300

    Fixed google map api key

--- app.config.js
+++ app.config.js
@@ -1,17 +1,19 @@
 module.exports = ({ config }) => {
   return {
+    ...config,
     ios: {
+      ...config.ios,
       config: {
         googleMapsApiKey: process.env.EXPO_PUBLIC_GOOGLE_API_KEY_IOS,
       },
     },
     android: {
+      ...config.android,
       config: {
         googleMaps: {
           apiKey: process.env.EXPO_PUBLIC_GOOGLE_API_KEY_ANDROID,
         },
       },
     },
-    ...config,
   };
 };
