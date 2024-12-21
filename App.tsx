89ed94870017f8a86cb2f5a25ab5989f2f8fc7b9
aki665 <aki.virtanen000@gmail.com>
2024-12-21 10:49:31 +0200

    Updated dependencies and patched ap.js to work on react native

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import "react-native-gesture-handler";
+import { StatusBar } from "expo-status-bar";
 import * as TaskManager from "expo-task-manager";
 import React from "react";
 import { View } from "react-native";
@@ -13,7 +14,12 @@ import ErrorContextProvider, { ErrorMessage } from "./components/ErrorContext";
 import SettingsContextProvider from "./components/SettingsContext";
 import ConnectTabs from "./screens/ConnectTabs";
 import Connected from "./screens/Connected";
+import structuredClone from "@ungap/structured-clone";
 require("react-native-get-random-values");
+
+if (!("structuredClone" in globalThis)) {
+  globalThis.structuredClone = structuredClone;
+}
 
 const Stack = createNativeStackNavigator();
 
@@ -27,6 +33,7 @@ function App() {
         <ClientContextProvider>
           <SettingsContextProvider>
             <View style={{ flex: 15 }}>
+              <StatusBar style="auto" />
               <NavigationContainer>
                 <Stack.Navigator
                   initialRouteName="connect"
