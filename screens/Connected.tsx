b7fd33bcf28231fb96ae2e8d95b397b1bfb680ce
aki665 <aki.virtanen000@gmail.com>
2024-12-24 08:49:36 +0200

    Fix chat scrolling

--- screens/Connected.tsx
+++ screens/Connected.tsx
@@ -326,8 +326,11 @@ export default function Connected({
       <Tab.Screen name="Chat">
         {(props) => (
           <ScrollView
-            refreshControl={<RefreshControl refreshing={reconnecting} />}
+            refreshControl={
+              <RefreshControl refreshing={reconnecting} enabled={false} />
+            }
             contentContainerStyle={{ flex: 1 }}
+            nestedScrollEnabled
           >
             {disconnected && (
               <Button
