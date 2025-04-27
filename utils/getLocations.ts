932aa8820b2595abbeb054c8e76b0014424d6edf
aki665 <aki.virtanen000@gmail.com>
2025-04-27 12:20:58 +0300

    Added more checks for invalid locations (osmID being 0)

--- utils/getLocations.ts
+++ utils/getLocations.ts
@@ -186,6 +186,18 @@ async function getLocationCoordinates(
     zoom,
     minDist,
   );
+  if (res.osmID === 0) {
+    res = await getLocationCoordinates(
+      latitude,
+      longitude,
+      maximum_distance,
+      distance_tier,
+      useNearZoom,
+      minimum_distance,
+      correction,
+      loop_count,
+    );
+  }
   const calculatedResult = Math.round(
     res.distance * 1000 * 1 + DISTANCE_LENIENCY,
   );
